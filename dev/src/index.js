702520ee88ff71b442a4df96c5fdd693f4b3925d
Carlos && Ponce <srtager555@gmail.com>
2022-01-25 19:11:56 -0600

    devStructure created

--- dev/src/index.js
+++ dev/src/index.js
@@ -2,6 +2,17 @@ import WrappingLetters from "../../src";
 import React from "react";
 import ReactDOM from "react-dom";
 
+function devStructure({ letter, cssClass, ...props }) {
+   const styles = {
+      display: "block",
+   }
+   return (
+      <span style={styles} className={cssClass} {...props}>
+         {`LETTER: ${letter}, CLASS: ${cssClass}`} 
+      </span>
+   );
+}
+
 function App() {
    return (
       <div className="App">
@@ -9,19 +20,15 @@ function App() {
             word="Hello world !!! <3"
             wordOptions={[
                {
-                  ClassToAdd: "class",
+                  // ClassToAdd: "class",
                   SelectClass: {
                      wordToSearch: "Hello",
                      classToAdd: "special-class",
-                     spaceBetweenWord: false,
+                     spaceBetweenWord: true,
                   },
                },
             ]}
-            // structure={(letter, cssClass, props) => (
-            //    <span className={cssClass} {...props}>
-            //       {letter}
-            //    </span>
-            // )}
+            structure={devStructure}
          />
       </div>
    );
