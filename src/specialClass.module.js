49b2cf52d1fa8770afc99a76408271dbf469fbc3
Carlos && Ponce <srtager555@gmail.com>
2022-02-04 19:59:18 -0600

    SBW is now optional and errors trap was optimized

--- src/specialClass.module.js
+++ src/specialClass.module.js
@@ -1,84 +1,82 @@
 import { WrappLetter } from './wrappLetter.module';
 
-function errorFilterSpecialClass(SelectClass) {
-    const SelectClassKeys = Object.keys(SelectClass);
-    const wl_props = ['ClassToAdd', 'SelectClass', 'PerWord'];
-    const containThisProps = (value) => wl_props.includes(value);
-    const container  = wordOptionsKeys.every(containThisProps);
-    
-    if(!container) {
-       throw new Error("wordOptions must contain the following properties: ClassToAdd, SelectClass, PerWord");
-    }
- 
-    const searchWord = SelectClassKeys.some((key) => key === "wordToSearch");
-    const searchWordClass = SelectClassKeys.some((key) => key === "classToAdd");
-    const searchSBW = SelectClassKeys.some((key) => key === "spaceBetweenWord");
- 
-    if (!searchWord && !searchWordClass && !searchSBW) {
-       throw new Error(
-          "SelectClass must have 3 keys : wordToSearch, spaceBetweenWord and classToAdd"
-       );
-    }
- 
-    if (
-       Object.prototype.toString.call(SelectClass.wordToSearch) !==
-          "[object String]" ||
-       Object.prototype.toString.call(SelectClass.spaceBetweenWord) !==
-          "[object Boolean]" ||
-       Object.prototype.toString.call(SelectClass.classToAdd) !==
-          "[object String]"
-    ) {
-       throw new Error(
-          "wordToSearch and classToAdd must be a string, spaceBetweenWord must be a boolean"
-       );
-    }
- }      
- 
- // ====== select special class to add ======
- export function selectSpecialClass({
-    SelectClass,
-    text,
-    ClassToAdd,
-    Structure,
-    specialStructure,
-    perWord,
- }) {
-    if (Object.keys(SelectClass).length !== 3) {
-       throw new Error(
-          "SelectClass must have 3 keys : wordToSearch, spaceBetween and classToAdd"
-       );
-    } else {
-       let searchWordValue;
-       let searchWordValueLength;
-       let specialClass;
- 
-       errorFilterSpecialClass(SelectClass);
-       searchWordValue = [...SelectClass.wordToSearch];
- 
-       if (SelectClass.spaceBetweenWord === true && perWord === false) {
-          searchWordValue.unshift(" ");
-          searchWordValue.push(" ");
-          text.push(" ");
+function errorFilterSpecialClass(SelectClass, perWord) {
+   const SelectClassKeys = Object.keys(SelectClass);
 
-          text.unshift(" ");
-       }
- 
-       searchWordValueLength = searchWordValue.length;
-       specialClass = SelectClass.classToAdd;
- 
-       return WrappLetter({
-          SelectClass: {
-             searchWordValue,
-             searchWordValueLength,
-             specialClass,
-             spaceBetweenWord: SelectClass.spaceBetweenWord,
-          },
-          text,
-          ClassToAdd,
-          Structure,
-          specialStructure,
-          perWord,
-       });
-    }
- }
+   const sc_props = ['wordToSearch', 'classToAdd', 'spaceBetweenWord'];
+
+   // required props 'wordToSearch', 'classToAdd'.
+   const containThisProps = (value) => sc_props.slice(0, 2).includes(value);
+   const contain = SelectClassKeys.every(containThisProps);
+
+   if (!contain && !perWord) {
+      throw new Error("\"SelectClass\" must contain the following properties: wordToSearch, classToAdd. spaceBetweenWord is optional");
+   }
+
+   const searchSBW = SelectClassKeys.some((key) => key === "spaceBetweenWord");
+
+   if (perWord && searchSBW) {
+      throw new Error("\"spaceBetweenWord\" is not allowed when perWord is true");
+   }
+   
+   const { wordToSearch, classToAdd, spaceBetweenWord } = SelectClass;
+   
+   if (typeof wordToSearch != "string") {
+      throw new Error("\"wordToSearch\" must be a string");
+   }
+   if (typeof classToAdd != "string") {
+      throw new Error("\"classToAdd\" must be a string");
+   }
+   if (!perWord && searchSBW && typeof spaceBetweenWord !== "boolean") {
+      throw new Error("\"spaceBetweenWord\" must be a boolean");
+   }
+}
+
+// ====== select special class to add ======
+export function selectSpecialClass({
+   SelectClass,
+   text,
+   ClassToAdd,
+   Structure,
+   specialStructure,
+   perWord,
+}) {
+   if (Object.keys(SelectClass).length !== 3 && false) {
+      throw new Error(
+         "SelectClass must have 3 keys : wordToSearch, spaceBetween and classToAdd"
+      );
+   } else {
+      let searchWordValue;
+      let searchWordValueLength;
+      let specialClass;
+
+      errorFilterSpecialClass(SelectClass, perWord);
+      searchWordValue = [...SelectClass.wordToSearch];
+
+      if (SelectClass.spaceBetweenWord === true && perWord === false) {
+         searchWordValue.unshift(" ");
+         searchWordValue.push(" ");
+         text.push(" ");
+
+         text.unshift(" ");
+      }
+
+      searchWordValueLength = searchWordValue.length;
+      specialClass = SelectClass.classToAdd;
+
+      return WrappLetter({
+         SelectClass: {
+            searchWordValue,
+            searchWordValueLength,
+            specialClass,
+            spaceBetweenWord: SelectClass.spaceBetweenWord,
+         },
+         text,
+         ClassToAdd,
+         Structure,
+         specialStructure,
+         perWord,
+      });
+   }
+}
  // ====== select special class to add ======
