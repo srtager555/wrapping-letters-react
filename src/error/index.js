318d8bcfc8110dad3a0cb67a061d1cace2f58c36
Carlos && Ponce <srtager555@gmail.com>
2022-07-12 20:24:46 -0600

    I updated the main error file

--- src/error/index.js
+++ src/error/index.js
@@ -1,8 +1,10 @@
-import { error__Filter_SpecialClass__ } from "./specialClass.error"
 import { whatItIs } from "../common/whatIsIt";
 
+import { error__Filter_SpecialClass__ } from "./specialClass.error"
+import { error__Filter_structure__ } from "./structure.error";
+
 export function error__main_filter__(wrappProps) {
-   const { ClassToAdd, SelectClass, SpecialWrapp, PerWord } = wrappProps;
+   const { ClassToAdd, SelectClass, SpecialWrapp, specialStructure, PerWord } = wrappProps;
 
    if (whatItIs(ClassToAdd) !== "[object String]")
       throw new Error("ClassToAdd must be a string");
@@ -11,4 +13,6 @@ export function error__main_filter__(wrappProps) {
       throw new Error("PerWord must be a boolean");
 
     error__Filter_SpecialClass__(SelectClass, PerWord)
+
+    error__Filter_structure__(ClassToAdd, specialStructure)
 }
