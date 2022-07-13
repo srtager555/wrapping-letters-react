fc235d94415e36582b73086229c35b888d8399d0
Carlos && Ponce <srtager555@gmail.com>
2022-07-12 20:21:51 -0600

    I updated the 'specialClass' to a process file

---
+++ src/specialClass.process.js
@@ -0,0 +1,22 @@
+export function process__select_specialClass__({ SelectClass, crumbledText, PerWord }) {
+   // Here the code will transform the user's code 
+   // to useful information to the code Xd
+
+   let searchWordValue = SelectClass.wordToSearch;
+   let specialClass;
+
+   // comprobation for search within words
+   if (SelectClass.spaceBetweenWord === true && PerWord === false) {
+      crumbledText.push(" ");
+
+      crumbledText.unshift(" ");
+   }
+
+   specialClass = SelectClass.classToAdd;
+
+   return {
+      searchWordValue,
+      specialClass,
+      spaceBetweenWord: SelectClass.spaceBetweenWord,
+   };
+}
