2f2b1bd69e6741cd3219f482557a7fa87a9a00ac
Carlos && Ponce <71289001+srtager555@users.noreply.github.com>
2022-07-17 21:14:18 -0600

    Jest integration. (#20)
    * I did install Jestjs for tests
    
    * I made a process folder
    
    * A short sentece
    
    * I upgraded  the import paths
    
    * I initializated the first test
    
    * I removed a useless log
    
    * I added a new test in the process
    
    * I  added new verification
    
    * Verification about if the crumbled text is returned with white space
    
    * I removed the white space in the start and end

---
+++ src/test/wrappLetter.process.test.js
@@ -0,0 +1,199 @@
+const wrappLetter = require("../process/wrappLetter.process");
+
+function structure({ letter, cssClass }) {
+  const Default = (
+    <div className="container">
+      <span className={cssClass}>{`LETTER: "${letter}", `}</span>
+      <span className={cssClass}>{`CLASS: "${cssClass}"`}</span>
+    </div>
+  );
+  return Default;
+}
+function baseStructure({ letter, cssClass, specialStructure = {} }) {
+  function DEFAULT_COMPONENT({ letter, cssClass }) {
+    return <span className={cssClass}>{letter}</span>;
+  }
+
+  const {
+    hasModification = false,
+    NewTagStructure = () => (
+      <DEFAULT_COMPONENT letter={letter} cssClass={cssClass} />
+    ),
+  } = specialStructure;
+
+  return hasModification ? (
+    <NewTagStructure />
+  ) : (
+    <DEFAULT_COMPONENT letter={letter} cssClass={cssClass} />
+  );
+}
+
+// if the user has a custom structure here it will be changed by it
+
+test("Should be return the same array (SBW: true)", () => {
+  const Structure = structure || baseStructure;
+
+  const crumbledText = [
+    " ",
+    "T",
+    "h",
+    "i",
+    "s",
+    " ",
+    "i",
+    "s",
+    " ",
+    "a",
+    " ",
+    "s",
+    "h",
+    "o",
+    "r",
+    "t",
+    " ",
+    "s",
+    "e",
+    "n",
+    "t",
+    "e",
+    "c",
+    "e",
+    " ",
+    "f",
+    "o",
+    "r",
+    " ",
+    "a",
+    "n",
+    " ",
+    "e",
+    "x",
+    "a",
+    "m",
+    "p",
+    "l",
+    "e",
+    " ",
+    "t",
+    "e",
+    "s",
+    "t",
+    " ",
+    "O",
+    "w",
+    "O",
+    " ",
+  ];
+
+  const arrWrapp = [
+    ["T", "class special-class-1"],
+    ["h", "class special-class-1"],
+    ["i", "class special-class-1"],
+    ["s", "class special-class-1"],
+    [" ", "class"],
+    ["i", "class special-class-1"],
+    ["s", "class special-class-1"],
+    [" ", "class"],
+    ["a", "class"],
+    [" ", "class"],
+    ["s", "class"],
+    ["h", "class"],
+    ["o", "class"],
+    ["r", "class"],
+    ["t", "class"],
+    [" ", "class"],
+    ["s", "class special-class-1"],
+    ["e", "class"],
+    ["n", "class"],
+    ["t", "class"],
+    ["e", "class"],
+    ["c", "class"],
+    ["e", "class"],
+    [" ", "class"],
+    ["f", "class"],
+    ["o", "class"],
+    ["r", "class"],
+    [" ", "class"],
+    ["a", "class"],
+    ["n", "class"],
+    [" ", "class"],
+    ["e", "class special-class-3"],
+    ["x", "class special-class-3"],
+    ["a", "class special-class-3"],
+    ["m", "class special-class-3"],
+    ["p", "class special-class-3"],
+    ["l", "class special-class-3"],
+    ["e", "class special-class-3"],
+    [" ", "class"],
+    ["t", "class"],
+    ["e", "class"],
+    ["s", "class"],
+    ["t", "class"],
+    [" ", "class"],
+    ["O", "class special-class-1"],
+    ["w", "class special-class-1"],
+    ["O", "class special-class-1"],
+  ];
+
+  const wrappResponse = wrappLetter.WrappLetter({
+    SelectClass: {
+      searchWordValue: ["OwO", "sentence", "example", "This", "is", "s"],
+      specialClass: ["special-class-1", "special-class-2", "special-class-3"],
+      spaceBetweenWord: true,
+    },
+    crumbledText,
+    ClassToAdd: "class",
+    Structure,
+    specialStructure: false,
+    PerWord: false,
+    test: true,
+  });
+
+  expect(wrappResponse).toStrictEqual(arrWrapp);
+});
+
+test("Should be return the same array (PerWord: true)", () => {
+  const Structure = structure || baseStructure;
+
+  const crumbledText = [
+    "This",
+    "is",
+    "a",
+    "short",
+    "sentece",
+    "for",
+    "an",
+    "example",
+    "test",
+    "OwO",
+  ];
+
+  const arrWrapp = [
+    ["This ", "class special-class-1"],
+    ["is ", "class special-class-1"],
+    ["a ", "class"],
+    ["short ", "class"],
+    ["sentece ", "class"],
+    ["for ", "class"],
+    ["an ", "class"],
+    ["example ", "class special-class-3"],
+    ["test ", "class"],
+    ["OwO", "class special-class-1"],
+  ];
+
+  const wrappResponse = wrappLetter.WrappLetter({
+    SelectClass: {
+      searchWordValue: ["OwO", "sentence", "example", "This", "is", "s"],
+      specialClass: ["special-class-1", "special-class-2", "special-class-3"],
+      spaceBetweenWord: false,
+    },
+    crumbledText,
+    ClassToAdd: "class",
+    Structure,
+    specialStructure: false,
+    PerWord: true,
+    test: true,
+  });
+
+  expect(wrappResponse).toStrictEqual(arrWrapp);
+});
