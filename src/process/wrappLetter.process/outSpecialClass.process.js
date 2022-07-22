5c72369d2e10ef7600064bae6972c475b305eb13
Carlos && Ponce <srtager555@gmail.com>
2022-07-21 21:41:47 -0600

    I updated the function for the new "function"

--- src/process/wrappLetter.process/outSpecialClass.process.js
+++ src/process/wrappLetter.process/outSpecialClass.process.js
@@ -1,32 +1,56 @@
+import { __specialArray__ } from "./specialArray.process";
+
 export function outSpecialClass({
   wrappElement,
   newClass,
   ClassToAdd,
-  specialArray,
+  SpecialWrapp,
   specialStructure,
+  specialClass,
   searchWordValue,
   index,
   crumbledText,
   PerWord,
 }) {
-  //here is wrapped per word.
-  let cssClass = !specialStructure ? ClassToAdd : "";
+  let { hasCustomWrapp, wordToWrapp, wrappToAdd } = SpecialWrapp;
 
+  let cssClass = !specialStructure ? ClassToAdd : "";
+  let word = wrappElement;
+  let customWrapp = undefined;
+
+  //here is wrapped per word.
   if (PerWord) {
     if (searchWordValue.includes(wrappElement)) {
       // here search the word in the array
       // and add the class
       const INDEX_SPECIAL_CLASS = searchWordValue.indexOf(wrappElement);
-      newClass = specialArray(INDEX_SPECIAL_CLASS);
+      newClass = __specialArray__(specialClass, INDEX_SPECIAL_CLASS);
 
       cssClass = !specialStructure
         ? [ClassToAdd, newClass].join(" ")
         : newClass;
     }
 
+    if (wordToWrapp.some((element) => element === word)) {
+      word = wordToWrapp.filter((el) => el === word);
+      hasCustomWrapp = true;
+      const WORD_INDEX = wordToWrapp.indexOf(word);
+
+      customWrapp = __specialArray__(wrappToAdd, WORD_INDEX);
+    }
+
     if (index != crumbledText.length - 1) wrappElement = wrappElement + " ";
     else wrappElement = wrappElement;
   }
 
-  return [[wrappElement, cssClass]];
+  return [
+    {
+      letter: wrappElement,
+      cssClass: cssClass,
+      specialWrapp: {
+        hasCustomWrapp,
+        NewWrappStructure: customWrapp,
+      },
+    },
+  ];
 }
