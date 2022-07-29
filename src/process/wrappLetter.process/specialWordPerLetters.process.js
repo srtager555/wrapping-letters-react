71f743c39df2b5a924e44df15e4f6228a3b114f9
Carlos && Ponce <srtager555@gmail.com>
2022-07-29 03:24:10 -0600

    new modifications added

--- src/process/wrappLetter.process/specialWordPerLetters.process.js
+++ src/process/wrappLetter.process/specialWordPerLetters.process.js
@@ -5,35 +5,64 @@ export function __specialWorld_PerLetters__({
   arrayOfWords,
   crumbledText,
   SpecialWrapp,
-  word,
+  searchWordValue,
+  spaceBetweenWord = false,
   specialStructure,
   newClass,
   ClassToAdd,
   specialClass,
 }) {
   let customWrapp = undefined;
+  let cssClass = !specialStructure ? ClassToAdd : "";
+
   let { hasCustomWrapp, wordToWrapp, wrappToAdd } = SpecialWrapp;
 
+  // here the code will filter the correct special "word"
+  // after the "word" will return within an Array
   let newCrumbledText = arrayOfWords.filter((element) => {
     // here the code will cut the "word" from the array
+
     let choppedWord = crumbledText
       .slice(index, index + element.length)
       .join("");
 
+    if (spaceBetweenWord) {
+      // here the code will checks if the "word" has whitespace
+      // before and after
+      if (crumbledText[index - 1] !== " ") return false;
+
+      if (crumbledText[index + element.length] !== " ") return false;
+    }
+
     return choppedWord === element;
   });
 
+  // here is the full word
+  let word = newCrumbledText[0];
+
+  // the code need the word separate per letter
   newCrumbledText = [...newCrumbledText[0]];
 
-  word = newCrumbledText.join("");
+  if (searchWordValue.includes(word)) {
+    // here search the word in the array
+    // and add the class
+    const INDEX_SPECIAL_CLASS = searchWordValue.indexOf(word);
+    newClass = __specialArray__(specialClass, INDEX_SPECIAL_CLASS);
 
-  if (arrayOfWords.some((element) => element === word)) {
+    cssClass = !specialStructure ? [ClassToAdd, newClass].join(" ") : newClass;
+  }
+
+  if (wordToWrapp.some((element) => element === word)) {
+    // here search the word in the array for
+    // add the customWrapp
     hasCustomWrapp = true;
     const WORD_INDEX = arrayOfWords.indexOf(word);
 
     customWrapp = __specialArray__(wrappToAdd, WORD_INDEX);
   }
 
+  // here the code will create magic
+  // with the before information the code can wrapp the elements
   var wl = newCrumbledText.map((wrappElement) => {
     const INDEX_SPECIAL_CLASS = arrayOfWords.indexOf(newCrumbledText.join(""));
 
@@ -41,7 +70,7 @@ export function __specialWorld_PerLetters__({
 
     return {
       letter: wrappElement,
-      cssClass: !specialStructure ? [ClassToAdd, newClass].join(" ") : newClass,
+      cssClass: cssClass,
       specialWrapp: {
         hasCustomWrapp,
         NewWrappStructure: customWrapp,
