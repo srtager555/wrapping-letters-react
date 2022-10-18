982805d1223ebe031d75cb21d1ef706474d19508
Carlos && Ponce <srtager555@gmail.com>
2022-10-17 19:30:43 -0600

    I added the Structure as a new props in an errror

--- src/error/index.js
+++ src/error/index.js
@@ -5,8 +5,14 @@ import { error__Filter_structure__ } from "./structure.error";
 import { error__Filter_SpecialWrapp__ } from "./specialWrapp.error";
 
 export function error__main_filter__(wrappProps) {
-  const { ClassToAdd, SelectClass, SpecialWrapp, specialStructure, PerWord } =
-    wrappProps;
+  const {
+    ClassToAdd,
+    SelectClass,
+    SpecialWrapp,
+    specialStructure,
+    Structure,
+    PerWord,
+  } = wrappProps;
 
   if (whatItIs(ClassToAdd) !== "[object String]")
     throw new Error("ClassToAdd must be a string");
@@ -18,5 +24,5 @@ export function error__main_filter__(wrappProps) {
 
   error__Filter_SpecialWrapp__(SpecialWrapp);
 
-  error__Filter_structure__(ClassToAdd, specialStructure);
+  error__Filter_structure__(ClassToAdd, specialStructure, Structure);
 }
