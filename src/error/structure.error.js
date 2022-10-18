59e840852092ac44daa319d4201908b38d29cc28
Carlos && Ponce <srtager555@gmail.com>
2022-10-17 19:30:02 -0600

    I added new errors to check if the values of the object are correct

--- src/error/structure.error.js
+++ src/error/structure.error.js
@@ -1,11 +1,17 @@
+import { whatItIs } from "../common/whatIsIt";
 import stringSimilarity from "string-similarity";
 
-export function error__Filter_structure__(ClassToAdd, specialStructure) {
+export function error__Filter_structure__(
+  ClassToAdd,
+  specialStructure,
+  Structure
+) {
   if (specialStructure) {
-    if (typeof specialStructure === "object") {
-      const STRUCTURE_KEYS = Object.keys(specialStructure);
+    if (typeof Structure === "object") {
+      const STRUCTURE_KEYS = Object.keys(Structure);
       const AVAILABLE_ATTRIBUTES = ["structure", "props"];
 
+      // here the code will iterate to search typing errors on the attributes
       STRUCTURE_KEYS.forEach((element) => {
         const CONDITIONAL = AVAILABLE_ATTRIBUTES.some((el) => el === element);
 
@@ -16,11 +22,29 @@ export function error__Filter_structure__(ClassToAdd, specialStructure) {
           );
 
           throw new Error(
-            `"${element}" isn't a registered property. Did you mean "${SS.bestMatch.target}"?`
+            `"${element}" isn't a registered attribute in the prop "structure" when is an Object. Did you mean "${SS.bestMatch.target}"?`
           );
         }
       });
+
+      // here the code will check if the attribute has the correct values
+      if (whatItIs(Structure.structure) != "[object Function]")
+        throw new Error(
+          `structure from the Object structure can't receive an ${whatItIs(
+            Structure.structure
+          )} as a value, the attribute structure should be a function/React Component`
+        );
+
+      if (whatItIs(Structure.props) != "[object Object]")
+        throw new Error("props from the Object structure must be an Object");
+
+      if (Object.keys(Structure.props).length === 0)
+        throw new Error(
+          "the attribute props from the Object structure is empty!!! if you don't want send props just need send the structure without an object"
+        );
     }
+
+    // here the code will check if textOptions has the attribute ClassToAdd
     if (ClassToAdd != "") {
       console.warn(
         "When the component has a 'special structure', 'ClassToAdd' is disabled"
