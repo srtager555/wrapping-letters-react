b6a89f5cfdc50ccf41dd59e84b41108ffabf9e05
Carlos && Ponce <srtager555@gmail.com>
2022-10-21 00:21:58 -0600

    I added the comprobation to the objects

--- src/error/specialWrapp.error.js
+++ src/error/specialWrapp.error.js
@@ -39,40 +39,83 @@ export function error__Filter_SpecialWrapp__(SpecialWrapp) {
   if (Array.isArray(structureToAdd)) {
     // here the will iterate the elements
     structureToAdd.forEach((element) => {
-      const __CONDITIONS__ = (target) => {
-        return [
-          whatItIs(target) != "[object Object]",
-          whatItIs(target) != "[object Function]",
-          !Array.isArray(target),
-        ];
-      };
+      const __CONDITIONS__ = (target) => [
+        whatItIs(target) != "[object Object]",
+        whatItIs(target) != "[object Function]",
+        !Array.isArray(target),
+      ];
 
+      // condi
       if (__CONDITIONS__(element).every((el) => el)) {
-        throw new Error(
-          `SpecialWrapp --- The Elements on "structureToAdd" must be an Array, Object or Function, You did put an ${whatItIs(
-            element
-          )}`
-        );
+        var err = `SpecialWrapp --- The Elements on "structureToAdd" must be an Array, Object or Function, You did put an ${whatItIs(
+          element
+        )}`;
+        throw new Error(err);
       }
 
-      if (Array.isArray(element)) {
+      if (Array.isArray(element))
         element.forEach((el) => {
           const CONDITIONS = __CONDITIONS__(el).pop();
 
           if (CONDITIONS.every((el) => el)) {
-            throw new Error(
-              `SpecialWrapp --- The Elements from an Array on "structureToAdd" must be an  Object or Function, You did put an ${whatItIs(
-                element
-              )}`
-            );
+            var err = `SpecialWrapp --- The Elements from an Array on "structureToAdd" must be an Object or Function, You did put an ${whatItIs(
+              element
+            )}`;
+            throw new Error(err);
           }
 
           if (whatItIs(el) === "[object Object]")
             __comprobation_correct_attributes__(el);
         });
-      }
     });
   }
 }
 
-function __comprobation_correct_attributes__(element) {}
+// ♻️ recicle code,
+function __comprobation_correct_attributes__(element) {
+  const ElementKeys = Object.keys(element);
+
+  // If SelectClass an empty Object the filter ends here
+  if (ElementKeys.length === 0)
+    return console.log('remove the empty object "{}"');
+
+  const sc_props = ["structureToAdd", "props"];
+
+  // required props 'structureToAdd', 'props'.
+  // comprobation if the object contain a props not in the list
+
+  let containThisProps = function containThisProps(value) {
+    return sc_props.includes(value);
+  };
+
+  let contain = ElementKeys.every(containThisProps);
+
+  // if the object doesn't contain the required props
+  if (!contain) {
+    throw new Error(
+      '"SelectClass" must contain the following properties: structureToAdd, props'
+    );
+  }
+
+  // if the object contain the required props
+  let wordToSearchSearch = ElementKeys.some(
+    (value) => value === "structureToAdd"
+  );
+  let classToAddSearch = ElementKeys.some((value) => value === "props");
+
+  if (!wordToSearchSearch || !classToAddSearch) {
+    throw new Error(
+      '"SelectClass" must be required the following properties: structureToAdd, props'
+    );
+  }
+
+  if (whatItIs(element.structureToAdd) != "[object Function]") {
+    let err = `SpecialWrapp - "structureToAdd" from an Object must be a Component/function `;
+    throw new Error(err);
+  }
+
+  if (whatItIs(element.props) != "[object Object]") {
+    let err = `SpecialWrapp - "props" from an Object must be an Object `;
+    throw new Error(err);
+  }
+}
