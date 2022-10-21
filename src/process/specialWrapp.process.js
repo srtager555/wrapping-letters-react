e6333727fb1f40a02a71b2a077722f9d13baa5c3
Carlos && Ponce <srtager555@gmail.com>
2022-10-21 04:01:37 -0600

    The wrapper don't use the object, only returns it when needed

--- src/process/specialWrapp.process.js
+++ src/process/specialWrapp.process.js
@@ -7,17 +7,18 @@ export function process__select_specialWrapp__({
   let props = {
     hasCustomWrapp: false,
     wordToWrapp: wordToSearch || new String(),
+    wrappToAdd: structureToAdd,
   };
 
   // here the code need a check if the attribute... wait a moment
 
-  if (whatItIs(structureToAdd) === "[object Function]")
-    props.props.wrappToAdd = {
-      structure: structureToAdd,
-      props: { AwAdeUwU: "uwu" },
-    };
+  if (whatItIs(structureToAdd) === "[object Object]")
+    props.wrappToAdd.props.awa = "uwu";
+
+  //the container does not process the object, it only returns it when needed
+
   // if it's not a function, it's an Object or an Array
-  else props.wrappToAdd = structureToAdd;
+  // else props.wrappToAdd = structureToAdd;
 
   if (!Array.isArray(props.wordToWrapp))
     props.wordToWrapp = [props.wordToWrapp];
