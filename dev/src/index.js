d8ebda7cdcecbb1629cafd50f3f27a678f25a3eb
Carlos && Ponce <srtager555@gmail.com>
2022-10-26 03:54:52 -0600

    Testing things... 5?

--- dev/src/index.js
+++ dev/src/index.js
@@ -5,25 +5,25 @@ import ReactDOM from "react-dom";
 
 import "./index.css";
 
-function specialTag({ letter, cssClass, color }) {
-  return (
-    <span className={cssClass}>
-      {letter}
-      {color}
-    </span>
-  );
+function specialTag({ letter, cssClass }) {
+  return <span className={cssClass}>{letter}</span>;
 }
 
-function structure({ letter, cssClass }) {
-  const Default = <span className={cssClass}>{letter}</span>;
-  return Default;
+function structure(props) {
+  console.log(props);
+  // const Default = <span className={cssClass}>{letter}</span>;
+  return <span>aaa</span>;
 }
 
 function App() {
   const [color, setColor] = useState(false);
+  const [value, setValue] = useState(structure);
+
+  console.log(value);
 
   const handleHover = () => {
     setColor(!color);
+    setValue(specialTag);
   };
 
   return (
@@ -36,26 +36,28 @@ function App() {
         <span>
           <WrappingLetters
             text="Hello <3"
-            textOptions={{
-              SelectClass: {
-                wordToSearch: ["Hello", "<3"],
-                classToAdd: ["aloh", "a"],
-              },
-              SpecialWrapp: {
-                wordToSearch: ["Hello", "<3"],
-                structureToAdd: [
-                  {
-                    structureToAdd: specialTag,
-                    props: { color: "." },
-                  },
-                  {
-                    structureToAdd: specialTag,
-                    props: { color: "2" },
-                  },
-                ],
-              },
-            }}
-            // structure={structure}
+            textOptions={
+              {
+                // SelectClass: {
+                //   wordToSearch: ["Hello", "<3"],
+                //   classToAdd: ["aloh", "a"],
+                // },
+                // SpecialWrapp: {
+                //   wordToSearch: ["Hello", "<3"],
+                //   structureToAdd: [
+                //     {
+                //       structureToAdd: specialTag,
+                //       props: { color: "." },
+                //     },
+                //     {
+                //       structureToAdd: specialTag,
+                //       props: { color: "2" },
+                //     },
+                //   ],
+                // },
+              }
+            }
+            structure={value}
             // structure={{
             //   structure: structure,
             //   props: { owo: "owo", awita: { a: "a " }, sex: "uwu" },
