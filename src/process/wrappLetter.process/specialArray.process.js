4724676744a49f26933989a45f6c36cad2c7cde9
Carlos && Ponce <srtager555@gmail.com>
2022-11-09 21:37:09 -0600

    I moved the function that iterate the array of string inside the main array

--- src/process/wrappLetter.process/specialArray.process.js
+++ src/process/wrappLetter.process/specialArray.process.js
@@ -1,3 +1,23 @@
+export function IterateOnAnArray(array, w) {
+  return array
+    .map((element, index) => {
+      if (Array.isArray(element)) {
+        if (
+          element.some((el) => {
+            return el === w;
+          })
+        ) {
+          return index;
+        }
+      }
+
+      if (element === w) return index;
+
+      return -1;
+    })
+    .find((el) => el != -1);
+}
+
 export function __specialArray__(specialArray, SPECIAL_INDEX) {
   if (Array.isArray(specialArray)) {
     if (SPECIAL_INDEX > specialArray.length - 1) {
