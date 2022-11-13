4a9fee0f3c8ff1aa9ec376940bb7f3cee04c438d
Carlos && Ponce <srtager555@gmail.com>
2022-11-13 15:49:45 -0600

    I moved the error filter of special wrapp to their class

--- src/error/index.js
+++ src/error/index.js
@@ -1,23 +1,11 @@
 import { whatItIs } from "../common/whatIsIt";
-
-import { error__Filter_SelectClass__ } from "./specialClass.error";
 import { error__Filter_structure__ } from "./structure.error";
-import { error__Filter_SpecialWrapp__ } from "./specialWrapp.error";
 
 export function error__main_filter__(wrappProps) {
-  const {
-    ClassToAdd,
-    SelectClass,
-    SpecialWrapp,
-    specialStructure,
-    Structure,
-    PerWord,
-  } = wrappProps;
+  const { ClassToAdd, specialStructure, Structure } = wrappProps;
 
   if (whatItIs(ClassToAdd) !== "[object String]")
     throw new Error("ClassToAdd must be a string");
-
-  error__Filter_SpecialWrapp__(SpecialWrapp);
 
   error__Filter_structure__(ClassToAdd, specialStructure, Structure);
 }
