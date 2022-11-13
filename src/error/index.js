e5cefc2da509b7fd4b649681f800e12787b238fb
Carlos && Ponce <srtager555@gmail.com>
2022-11-13 17:17:11 -0600

    I moved the ClassToAdd error to their class

--- src/error/index.js
+++ src/error/index.js
@@ -4,8 +4,5 @@ import { error__Filter_structure__ } from "./structure.error";
 export function error__main_filter__(wrappProps) {
   const { ClassToAdd, specialStructure, Structure } = wrappProps;
 
-  if (whatItIs(ClassToAdd) !== "[object String]")
-    throw new Error("ClassToAdd must be a string");
-
   error__Filter_structure__(ClassToAdd, specialStructure, Structure);
 }
