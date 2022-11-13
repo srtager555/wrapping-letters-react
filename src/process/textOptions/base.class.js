22c40919207cddbcb1f65d7c6ee9038d6f9f075d
Carlos && Ponce <srtager555@gmail.com>
2022-11-13 03:44:30 -0600

    Mmmh...

--- src/process/textOptions/base.class.js
+++ src/process/textOptions/base.class.js
@@ -1,8 +1,19 @@
 export class Base {
-  constructor({ SpecialArray, Props, __error, __process }) {
-    this.SpecialArray = SpecialArray || false;
+  constructor({ SpecialArray = false, Props = false, __error, __process }) {
     this.Props = Props || false;
     this.__error = __error;
     this.__process = __process;
+
+    if (SpecialArray) {
+      this.SpecialArray = this.#SpecialArray;
+    }
+
+    if (Props) {
+      this.SpecialArray = this.#SpecialArray;
+    }
+  }
+
+  #SpecialArray() {
+    console.log("a");
   }
 }
