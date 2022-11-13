2fb901021bcba3280c9edb6224a9b3d5bc71e3ba
Carlos && Ponce <srtager555@gmail.com>
2022-11-12 23:21:37 -0600

    I removed the SelectClass and PerWord error

--- src/error/index.js
+++ src/error/index.js
@@ -17,11 +17,6 @@ export function error__main_filter__(wrappProps) {
   if (whatItIs(ClassToAdd) !== "[object String]")
     throw new Error("ClassToAdd must be a string");
 
-  if (whatItIs(PerWord) !== "[object Boolean]")
-    throw new Error("PerWord must be a boolean");
-
-  error__Filter_SelectClass__(SelectClass, PerWord);
-
   error__Filter_SpecialWrapp__(SpecialWrapp);
 
   error__Filter_structure__(ClassToAdd, specialStructure, Structure);
