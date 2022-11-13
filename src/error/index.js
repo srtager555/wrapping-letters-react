e9c566f9f85cb7b95cfc997486a2e9b45c1f8647
Carlos && Ponce <srtager555@gmail.com>
2022-11-13 17:29:23 -0600

    I removed the structure error

--- src/error/index.js
+++ src/error/index.js
@@ -3,6 +3,4 @@ import { error__Filter_structure__ } from "./structure.error";
 
 export function error__main_filter__(wrappProps) {
   const { ClassToAdd, specialStructure, Structure } = wrappProps;
-
-  error__Filter_structure__(ClassToAdd, specialStructure, Structure);
 }
