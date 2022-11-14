f1f87ab7b549acd29a3e8cd6bd23b2e9ad523c1d
Carlos && Ponce <srtager555@gmail.com>
2022-11-14 03:38:19 -0600

    I'm working in this to work in the wrapper

--- src/process/textOptions/base.class.js
+++ src/process/textOptions/base.class.js
@@ -1,19 +1,23 @@
+import { __specialArray__, IterateOnAnArray } from "./specialArray.process";
 export class Base {
   constructor({ SpecialArray = false, Props = false, __error, __process }) {
-    this.Props = Props || false;
     this.__error = __error;
     this.__process = __process;
+    this.SpecialArray = SpecialArray;
 
-    if (SpecialArray) {
-      this.SpecialArray = this.#SpecialArray;
+    if (this.SpecialArray) {
+      this.findTarget = (target) => this.#FIND_TARGET(target);
     }
 
     if (Props) {
-      this.SpecialArray = this.#SpecialArray;
+      // this.SpecialArray = this.#SPECIAL_ARRAY;
     }
   }
 
-  #SpecialArray() {
-    console.log("a");
+  #FIND_TARGET(target) {
+    if (!this.SpecialArray.targets.flat().includes(target)) return undefined;
+
+    const TARGET_INDEX = IterateOnAnArray(this.SpecialArray.targets, target);
+    // custom process if the target is true
   }
 }
