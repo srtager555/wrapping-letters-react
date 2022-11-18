85314db53c4456dcba71c2c5765c3cd31dc5eb0a
Carlos && Ponce <srtager555@gmail.com>
2022-11-17 20:19:13 -0600

    first part to the new wrap created

--- src/process/wrappLetter.process/index.js
+++ src/process/wrappLetter.process/index.js
@@ -10,27 +10,24 @@ export function WrappLetter(TEXT_OPTIONS, STRUCTURE) {
   const { crumbledText, ClassToAdd, SelectClass, SpecialWrapp, PerWord } =
     TEXT_OPTIONS;
 
-  const CustomComponent = STRUCTURE.currrent.structure.structure;
+  const CustomComponent = STRUCTURE.getStructure.structure;
   const CustomProps = STRUCTURE.current.props;
-  const searchWordValueWithoutArrays = searchWordValue.flat();
+
+  console.log(crumbledText);
 
   var arrElements = crumbledText
     .map((wrappElement, index) => {
       const WL = new LettersWrapping(wrappElement, index, TEXT_OPTIONS);
-      let newClass = specialClass;
-      let word;
 
       // This function has the work find the specialClass with the index; Here the code will start the comprobations
-      const COMPROBATIONS = WL.Comprobation(
-        PerWord,
-        searchWordValueWithoutArrays
-      );
 
-      if (COMPROBATIONS && WL.SPW(SelectClass.process.spaceBetweenWord)) {
-        word; // = neCrumbletText
-      } else {
-        word = wrappElement;
-      }
+      const VALUES_TO_ADD = TEXT_OPTIONS.takeAttributesTheySeek.map((el) => {
+        const VALUE = WL.getSpecialArrayResults(el.SpecialArray);
+
+        return el.findTarget(VALUE);
+      });
+
+      console.log(VALUES_TO_ADD);
     })
     .flat();
 
