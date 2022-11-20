1bfac576f184136fbac27f004783db400a6d8984
Carlos && Ponce <srtager555@gmail.com>
2022-11-20 04:06:35 -0600

    Useless file

--- src/error/index.js
+++
@@ -1,6 +0,0 @@
-import { whatItIs } from "../common/whatIsIt";
-import { error__Filter_structure__ } from "./structure.error";
-
-export function error__main_filter__(wrappProps) {
-  const { ClassToAdd, specialStructure, Structure } = wrappProps;
-}
