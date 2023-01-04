e7388888935e66d9fb8cfb8b27c7d5f7948ad919
Carlos && Ponce <srtager555@gmail.com>
2023-01-04 00:29:37 -0600

    I added the CustomComponent here

--- src/test/textArrays.js
+++ src/test/textArrays.js
@@ -1,7 +1,23 @@
+import React from "react";
+
 const sentenceToTest = "This is a sentence!!!";
 
 const sentencePerWord = sentenceToTest.split(" ");
 
 const sentencePerLetter = [...sentenceToTest];
 
-export { sentencePerWord, sentencePerLetter };
+// eslint-disable-next-line react/prop-types
+function CustomStructure({ letter, cssClass }) {
+  return <span className={cssClass}>{`LETTER: "${letter}", `}</span>;
+}
+function CustomStructure1({ letter, cssClass }) {
+  return <span className={cssClass}>{`LETTER: "${letter}", `}</span>;
+}
+
+export {
+  sentenceToTest,
+  sentencePerWord,
+  sentencePerLetter,
+  CustomStructure,
+  CustomStructure1,
+};
