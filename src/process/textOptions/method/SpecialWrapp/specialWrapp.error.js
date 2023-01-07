3641e8e56624951ab123cc1d7c42712eeed66279
Carlos && Ponce <srtager555@gmail.com>
2023-01-07 01:45:10 -0600

    I updated the deprecate SpecialWrapp errors filter

--- src/process/textOptions/method/SpecialWrapp/specialWrapp.error.js
+++ src/process/textOptions/method/SpecialWrapp/specialWrapp.error.js
@@ -1,98 +1,74 @@
 import { whatItIs } from "../../../../common/whatIsIt";
+import { BaseErrorSeekers } from "../../../../error/baseError-seekers.class";
 
-export function error__Filter_SpecialWrapp__(SpecialWrapp) {
-  if (whatItIs(SpecialWrapp) != "[object Object]")
-    throw new Error("SpecialWrapp must be an Object --- Wrapping Letters");
+export function error__Filter_SpecialWrapp__(SpecialWrapp, PerWord) {
+  new BaseErrorSeekers({
+    entry: SpecialWrapp,
+    Attributes: null,
+    PerWord,
+    customError: () => custom(),
+  });
 
-  if (Object.keys(SpecialWrapp).length === 0) return;
+  function custom() {
+    const { structureToAdd } = SpecialWrapp;
 
-  const { wordToSearch, structureToAdd } = SpecialWrapp;
+    // from now on the code will check if
+    // the attribute has the correct conditions
 
-  // missing the check if the object has the 2 correct attributes
+    if (
+      whatItIs(structureToAdd) != "[object Object]" &&
+      whatItIs(structureToAdd) != "[object Function]" &&
+      !Array.isArray(structureToAdd)
+    ) {
+      throw new Error(
+        'SpecialWrapp --- "structureToAdd" must be an Array, Object or Function'
+      );
+    }
 
-  if (
-    whatItIs(wordToSearch) != "[object String]" &&
-    !Array.isArray(wordToSearch)
-  ) {
-    throw new Error(
-      'SpecialWrapp --- "wordToSearch" must be an Array or String'
-    );
-  }
+    // here the code will verify that the object has the correct attributes
+    if (whatItIs(structureToAdd) === "[object Object]")
+      __comprobation_correct_attributes__(structureToAdd);
 
-  // from now on the code will check if
-  // the attribute has the correct conditions
+    // here the code will check each element from an Array
+    if (Array.isArray(structureToAdd)) {
+      // here the will iterate the elements
+      structureToAdd.forEach((element) => {
+        // here the code will know if the element has the correct element inside it
+        if (
+          whatItIs(element) != "[object Object]" &&
+          whatItIs(element) != "[object Function]"
+        ) {
+          var err = `SpecialWrapp --- The Elements on the Array from "structureToAdd" must be an Object or Component, You did put an ${whatItIs(
+            element
+          )}`;
+          throw new Error(err);
+        }
 
-  if (
-    whatItIs(structureToAdd) != "[object Object]" &&
-    whatItIs(structureToAdd) != "[object Function]" &&
-    !Array.isArray(structureToAdd)
-  ) {
-    throw new Error(
-      'SpecialWrapp --- "structureToAdd" must be an Array, Object or Function'
-    );
-  }
-
-  // here the code will verify that the object has the correct attributes
-  if (whatItIs(structureToAdd) === "[object Object]")
-    __comprobation_correct_attributes__(structureToAdd);
-
-  // here the code will check each element from an Array
-  if (Array.isArray(structureToAdd)) {
-    // here the will iterate the elements
-    structureToAdd.forEach((element) => {
-      const __CONDITIONS__ = (target) => [
-        whatItIs(target) != "[object Object]",
-        whatItIs(target) != "[object Function]",
-      ];
-
-      // here the code will know if the element has the correct element inside it
-      if (__CONDITIONS__(element).every((el) => el)) {
-        var err = `SpecialWrapp --- The Elements on "structureToAdd" must be an Array, Object or Function, You did put an ${whatItIs(
-          element
-        )}`;
-        throw new Error(err);
-      }
-    });
+        if (whatItIs(element) === "[object Object]")
+          __comprobation_correct_attributes__(element);
+      });
+    }
   }
 }
 
-// ♻️ recicle code,
 function __comprobation_correct_attributes__(element) {
-  const ElementKeys = Object.keys(element);
+  const ELEMENT_KEYS = Object.keys(element);
 
   // If SelectClass an empty Object the filter ends here
-  if (ElementKeys.length === 0)
+  if (ELEMENT_KEYS.length === 0)
     throw new Error(
       'SpecialWrapp - The Wrapper can process empty objects on strutureToAdd from SpecialWrapp "{}"'
     );
 
-  const sc_props = ["structureToAdd", "props"];
-
-  // required props 'structureToAdd', 'props'.
-  // comprobation if the object contain a props not in the list
-
-  let containThisProps = function containThisProps(value) {
-    return sc_props.includes(value);
-  };
-
-  let contain = ElementKeys.every(containThisProps);
-
-  // if the object doesn't contain the required props
-  if (!contain) {
+  if (!ELEMENT_KEYS["structureoAdd"]) {
     throw new Error(
-      '"SpecialWrapp" must contain the following properties: structureToAdd, props'
+      '"SpecialWrapp" - if you want send props to you wrap, the object need the Attribute `structureToAdd<Component>`'
     );
   }
 
-  // if the object contain the required props
-  let wordToSearchSearch = ElementKeys.some(
-    (value) => value === "structureToAdd"
-  );
-  let classToAddSearch = ElementKeys.some((value) => value === "props");
-
-  if (!wordToSearchSearch || !classToAddSearch) {
+  if (!ELEMENT_KEYS["props"]) {
     throw new Error(
-      '"SpecialWrapp" must be required the following properties: structureToAdd, props'
+      '"SpecialWrapp" - if you want send props to you wrap, the object need the Attribute `props<Object>`'
     );
   }
 
